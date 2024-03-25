13d6550b66a0bd776d821f3f657d8f63e656c760
Hamza Shabbir <hamza.shabbir@qpharmacorp.com>
2024-03-25 10:29:56 +0500

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ReportComponent } from './components/report/report.component';
+import { DocumentComponent } from './components/document/document.component';
+import { HistoryComponent } from './components/history/history.component';
+import { SettingComponent } from './components/setting/setting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
+    ReportComponent,
+    DocumentComponent,
+    HistoryComponent,
+    SettingComponent
   ],
   imports: [
     BrowserModule,
